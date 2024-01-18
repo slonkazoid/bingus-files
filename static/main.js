d0411c8da215bc5ac2396c0da3f952274eb33cfa
slonkazoid <alifurkanyildiz@gmail.com>
2024-01-19 02:39:38 +0300

    switch to XMLHttpRequest permanently

--- static/main.js
+++ static/main.js
@@ -102,148 +102,105 @@ fileInput.addEventListener("change", async (e) => {
 		)})`;
 		messages[i].replaceWith(logged);
 
-		if (form.progress.checked) {
-			// upload with progress (sigh)
+		// upload with progress (sigh)
 
-			let div = document.createElement("div");
+		let div = document.createElement("div");
 
-			let localProgressBar = document.createElement("progress");
-			localProgressBar.style.width = "50%";
-			localProgressBar.style.display = "inline";
-			localProgressBar.max = file.size;
-			localProgressBar.value = "0";
-			div.append(localProgressBar);
+		let localProgressBar = document.createElement("progress");
+		localProgressBar.style.width = "50%";
+		localProgressBar.style.display = "inline";
+		localProgressBar.max = file.size;
+		localProgressBar.value = "0";
+		div.append(localProgressBar);
 
-			let progressText = document.createElement("span");
-			let prematureOptimization = prettyFileSize(file.size);
-			progressText.innerText = `0 B/${prematureOptimization}`;
-			progressText.style.float = "right";
-			progressText.style["margin-left"] = "0.5em";
-			progressText.title = "rate is sampled from last 15 seconds";
-			div.append(progressText);
+		let progressText = document.createElement("span");
+		let prematureOptimization = prettyFileSize(file.size);
+		progressText.innerText = `0 B/${prematureOptimization}`;
+		progressText.style.float = "right";
+		progressText.style["margin-left"] = "0.5em";
+		progressText.title = "rate is sampled from last 15 seconds";
+		div.append(progressText);
 
-			let /** @type {XMLHttpRequest} */ res;
-			try {
-				// i love callbacks
-				res = await new Promise((resolve, reject) => {
-					/**
-					 * @typedef {Object} rateSnapshot
-					 * @property {number} ts
-					 * @property {number} total
-					 */
-					let /** @type {rateSnapshot[]} */ rateCalcMagicArray = [];
+		let /** @type {XMLHttpRequest} */ res;
+		try {
+			// i love callbacks
+			res = await new Promise((resolve, reject) => {
+				/**
+				 * @typedef {Object} rateSnapshot
+				 * @property {number} ts
+				 * @property {number} total
+				 */
+				let /** @type {rateSnapshot[]} */ rateCalcMagicArray = [];
 
-					let /** @type {number} */ uploadStart;
+				let /** @type {number} */ uploadStart;
 
-					let req = new XMLHttpRequest();
-					req.open("PUT", "/" + file.name);
-					req.upload.addEventListener("progress", (e) => {
-						console.log("progress event", e);
-						localProgressBar.value = e.loaded;
-						let ratio = e.loaded / file.size;
-						let now = Date.now();
-						rateCalcMagicArray.push({
-							ts: now,
-							total: e.loaded,
-						});
-						let firstSnapshot = rateCalcMagicArray.filter(
-							(snapshot) => snapshot.ts + 15 * 1000 >= now
-						)[0];
-						let rate =
-							firstSnapshot === undefined
-								? 0
-								: (e.loaded - firstSnapshot.total) /
-								  (now - firstSnapshot.ts);
-
-						progressText.innerText = `${prettyFileSize(
-							e.loaded
-						)}/${prematureOptimization} (${(ratio * 100).toFixed(
-							2
-						)}%) ${prettyFileSize(rate * 1000)}/s`;
+				let req = new XMLHttpRequest();
+				req.open("PUT", "/" + file.name);
+				req.upload.addEventListener("progress", (e) => {
+					console.log("progress event", e);
+					localProgressBar.value = e.loaded;
+					let ratio = e.loaded / file.size;
+					let now = Date.now();
+					rateCalcMagicArray.push({
+						ts: now,
+						total: e.loaded,
 					});
-					req.addEventListener("loadstart", () => {
-						uploadStart = Date.now();
-						rateCalcMagicArray.push({
-							ts: uploadStart,
-							total: 0,
-						});
-						logged.appendChild(div);
-					});
-					req.addEventListener("error", () => reject(req));
-					req.addEventListener("abort", () => reject(req));
-					req.addEventListener("load", () => resolve(req));
-					req.addEventListener("loadend", () => div.remove());
-					req.send(file);
+					let firstSnapshot = rateCalcMagicArray.filter(
+						(snapshot) => snapshot.ts + 15 * 1000 >= now
+					)[0];
+					let rate =
+						firstSnapshot === undefined
+							? 0
+							: (e.loaded - firstSnapshot.total) /
+							  (now - firstSnapshot.ts);
+
+					progressText.innerText = `${prettyFileSize(
+						e.loaded
+					)}/${prematureOptimization} (${(ratio * 100).toFixed(
+						2
+					)}%) ${prettyFileSize(rate * 1000)}/s`;
 				});
-			} catch (err) {
-				errors++;
-				p.append(
-					`failed to upload file. see the console for more details`
-				);
-				console.error(err);
-				continue;
-			}
+				req.addEventListener("loadstart", () => {
+					uploadStart = Date.now();
+					rateCalcMagicArray.push({
+						ts: uploadStart,
+						total: 0,
+					});
+					logged.appendChild(div);
+				});
+				req.addEventListener("error", () => reject(req));
+				req.addEventListener("abort", () => reject(req));
+				req.addEventListener("load", () => resolve(req));
+				req.addEventListener("loadend", () => div.remove());
+				req.send(file);
+			});
+		} catch (err) {
+			errors++;
+			p.append(`failed to upload file. see the console for more details`);
+			console.error(err);
+			continue;
+		}
 
-			if (res.status === 200) {
-				let fileName = res.responseText;
-				let p = document.createElement("p");
-				p.append("uploaded ");
-				let a = document.createElement("a");
-				a.href = `${location.origin}/${fileName}`;
-				a.innerText = fileName;
-				p.append(a);
-				p.append(` (${prettyMs(performance.now() - start)})`);
-				logged.replaceWith(p);
-				progressBar.value = parseFloat(progressBar.value) + 1;
-			} else {
-				errors++;
-				console.error(res.status, res.statusText);
-				let p = document.createElement("p");
-				p.append(
-					`failed to upload file: ${res.statusText}. see the console for more details`
-				);
-				logged.replaceWith(p);
-			}
+		if (res.status === 200) {
+			let fileName = res.responseText;
+			let p = document.createElement("p");
+			p.append("uploaded ");
+			let a = document.createElement("a");
+			a.href = `${location.origin}/${fileName}`;
+			a.innerText = fileName;
+			p.append(a);
+			p.append(` (${prettyMs(performance.now() - start)})`);
+			logged.replaceWith(p);
+			progressBar.value = parseFloat(progressBar.value) + 1;
+			updateStats();
 		} else {
-			// upload without torturing self
-
-			let /** @type {Response} */ res;
-			try {
-				res = await fetch("/" + file.name, {
-					method: "PUT",
-					body: file,
-				});
-			} catch (err) {
-				errors++;
-				console.error(err);
-				let p = document.createElement("p");
-				p.append(
-					`failed to upload file. see the console for more details`
-				);
-				logged.replaceWith(p);
-				continue;
-			}
-
-			if (res.status === 200) {
-				let fileName = await res.text();
-				let p = document.createElement("p");
-				p.append("uploaded ");
-				let a = document.createElement("a");
-				a.href = `${location.origin}/${fileName}`;
-				a.innerText = fileName;
-				p.append(a);
-				p.append(` (${prettyMs(performance.now() - start)})`);
-				logged.replaceWith(p);
-				progressBar.value = parseFloat(progressBar.value) + 1;
-			} else {
-				errors++;
-				console.error(res.status, res.statusText);
-				let p = document.createElement("p");
-				p.append(
-					`failed to upload file: ${res.statusText}. see the console for more details`
-				);
-				logged.replaceWith(p);
-			}
+			errors++;
+			console.error(res.status, res.statusText);
+			let p = document.createElement("p");
+			p.append(
+				`failed to upload file: ${res.statusText}. see the console for more details`
+			);
+			logged.replaceWith(p);
 		}
 	}
 
